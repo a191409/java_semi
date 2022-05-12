ef9b869dcdfc9f50d208d45f26a08e857bfb21ea
佐藤綜一郎 <sato.soichiro@unizonbex.com>
2022-05-12 19:20:03 +0900

    Userクラスにname属性を追加

--- src/main/java/model/user/User.java
+++ src/main/java/model/user/User.java
@@ -5,11 +5,13 @@ import model.Default;
 import java.sql.Timestamp;
 
 public class User extends Default {
+    private String name;
     private String email;
     private String pass;
 
     public User(
             Integer id,
+            String name,
             String email,
             String pass,
             Timestamp createdAt,
@@ -17,11 +19,16 @@ public class User extends Default {
     ){
         //親クラス(Default)のコンストラクタを呼び出す
         super(id, createdAt, updatedAt);
+        this.name=  name;
         this.email = email;
         this.pass = pass;
     }
 
     //setメソッド
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -32,11 +39,26 @@ public class User extends Default {
 
 
     //getメソッド
+    public String getName() {
+        return name;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public String getPass() {
         return pass;
+    }
+
+    // 新規登録用メソッド
+    public void signUpUser() {
+        Repository.signUpUser(this);
+    }
+
+    // ログイン用メソッド
+    public User login() {
+        // emailをもとにユーザーが存在するかどうか調べる
+        return Repository.selectUserByEmail(this.email);
     }
 }
