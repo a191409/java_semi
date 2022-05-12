2c71a38a5561279c0d0d11085b1131e4ec5feee1
佐藤綜一郎 <sato.soichiro@unizonbex.com>
2022-05-13 01:35:56 +0900

    入力を受け付けて完了後の画面遷移まで実装

--- src/main/java/model/category/Repository.java
+++ src/main/java/model/category/Repository.java
@@ -2,5 +2,34 @@ package model.category;
 
 import lib.mysql.Client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 public class Repository extends Client {
+    public static void insert(Category category) {
+        Connection connection = null;
+        PreparedStatement stmt = null;
+
+        try {
+            String sql = "insert into categories (name, created_at, updated_at) values (?, ?, ?)";
+
+            connection = create();
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, category.getName());
+            stmt.setTimestamp(2, currentTime);
+            stmt.setTimestamp(3, currentTime);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(connection, stmt, null);
+        }
+    }
+
 }
