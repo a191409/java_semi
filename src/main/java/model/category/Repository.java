d1a8d7d3f083df2055a4179134eec8818063d30b
佐藤綜一郎 <sato.soichiro@unizonbex.com>
2022-05-13 14:55:28 +0900

    insertメソッド実装

--- src/main/java/model/category/Repository.java
+++ src/main/java/model/category/Repository.java
@@ -2,10 +2,8 @@ package model.category;
 
 import lib.mysql.Client;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Repository extends Client {
     public static void insert(Category category) {
@@ -32,4 +30,35 @@ public class Repository extends Client {
         }
     }
 
+    public static ArrayList<Category> indexCategories() {
+        Connection connection = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = "select * from categories";
+
+            connection = create();
+            stmt = connection.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            ArrayList<Category> categories = new ArrayList<>();
+            while (rs.next()) {
+                Category category = new Category(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        null,
+                        null
+                );
+                categories.add(category);
+            }
+            return categories;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            close(connection, stmt, rs);
+        }
+    }
 }
